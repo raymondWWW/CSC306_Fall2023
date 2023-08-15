b2b40ec416f098061159247ee66bf53ab497b1f5
raymondWWW <raymondWWW@gmail.com>
2023-08-15 15:59:34 -0700

    Update easydraw.py
    Signed-off-by: raymondWWW <raymondWWW@gmail.com>

--- easydraw.py
+++ easydraw.py
@@ -1,38 +1,57 @@
-# Just a wrapper for drawSVG that uses global variables so that things are simplified a bit
-!pip install drawSvg==1.2.2 numpy==1.21.1
+# easydraw.py
+
+# Ensure required packages are installed. In the Colab notebook, run:
+# !pip install drawSvg==1.2.2 numpy==1.21.1
 
 import drawSvg as dSVG
 
-def easydraw(*args,**kwargs):
-    global drawSVG_easydraw
-    drawSVG_easydraw = dSVG.Drawing(*args, **kwargs)
+_drawSVG_easydraw = None
+
+def _ensure_initialized():
+    global _drawSVG_easydraw
+    if _drawSVG_easydraw is None:
+        raise Exception("Please initialize using easydraw() before drawing.")
+
+def easydraw(*args, **kwargs):
+    global _drawSVG_easydraw
+    _drawSVG_easydraw = dSVG.Drawing(*args, **kwargs)
 
 def Text(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Text(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Text(*args, **kwargs))
 
 def Rectangle(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Rectangle(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Rectangle(*args, **kwargs))
 
 def Circle(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Circle(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Circle(*args, **kwargs))
 
 def Ellipse(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Ellipse(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Ellipse(*args, **kwargs))
 
 def ArcLine(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.ArcLine(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.ArcLine(*args, **kwargs))
 
 def Path(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Path(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Path(*args, **kwargs))
 
 def Lines(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Lines(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Lines(*args, **kwargs))
 
 def Line(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Line(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Line(*args, **kwargs))
 
 def Arc(*args, **kwargs):
-    drawSVG_easydraw.append(dSVG.Arc(*args, **kwargs))
+    _ensure_initialized()
+    _drawSVG_easydraw.append(dSVG.Arc(*args, **kwargs))
 
 def show_me_my_drawing():
-    return drawSVG_easydraw
+    _ensure_initialized()
+    return _drawSVG_easydraw
